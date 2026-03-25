1415d5f2b2d2df7c280f1eb3488504ba3621984d
gghjik41-droid <gghjik41@gmail.com>
2026-03-26 01:00:52 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Меняй это число при каждом обновлении, чтобы сбросить кэш
-const CACHE_VERSION = 11;
+const CACHE_VERSION = 12;
 const CACHE_NAME = 'pso-v' + CACHE_VERSION;
 
 // Список файлов для оффлайн-режима
