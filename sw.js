5cf77e851e0ea9313f7ca938fef961029863b272
gghjik41-droid <gghjik41@gmail.com>
2026-03-29 18:55:41 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Меняй это число при каждом обновлении, чтобы сбросить кэш
-const CACHE_VERSION = 23;
+const CACHE_VERSION = 24;
 const CACHE_NAME = 'pso-v' + CACHE_VERSION;
 
 // Список файлов для оффлайн-режима
