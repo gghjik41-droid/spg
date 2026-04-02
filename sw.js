91301a8b05bae475fdfb2988eb7788fb6e260a7d
gghjik41-droid <gghjik41@gmail.com>
2026-04-03 03:16:47 +0700

    Revert "v45"
    This reverts commit 3dedf832a5cbc0658a21659bb1ffbf96a54da97f.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Меняй это число при каждом обновлении, чтобы сбросить кэш
-const CACHE_VERSION = 45;
+const CACHE_VERSION = 38;
 const CACHE_NAME = 'pso-v' + CACHE_VERSION;
 
 // Список файлов для оффлайн-режима
